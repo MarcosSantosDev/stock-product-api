9c0d8259c19fcc906e9272d5114f78d349e2ebbd
Marcos Santos <marcosadriano034@gmail.com>
2018-11-11 15:41:08 -0200

    connecting to the database

--- src/server.js
+++ src/server.js
@@ -1,14 +1,22 @@
 const express = require('express');
 const http = require('http');
 const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const server = express();
 
+mongoose.connect('mongodb://localhost/stockproject');
+
 const indexRouter = require('../routes/index');
+
+server.set('port', process.env.PORT || 3000);
 
 server.use(morgan('dev'));
 
-server.set('port', process.env.PORT || 3000);
+server.use(bodyParser.json());
+
+server.use(bodyParser.urlencoded({ extended: false }));
 
 server.get('/', indexRouter);
 
